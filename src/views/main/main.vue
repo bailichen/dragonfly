ed55d753ff1a692d44666ef2441826c239367f30
xulianjie@landawang.com <xulianjie@landawang.com>
2019-04-18 10:39:36 +0800

    初始化样式

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,21 +1,29 @@
 <template>
     <div>
-        <frame-view-float title="微信测试" :z="120" class="main">
-                微信测试
+        <frame-view-float title="微信测试"
+                          :z="120"
+                          class="main">
+
+            <div class="main-content">微信测试</div>
         </frame-view-float>
     </div>
 </template>
 <script>
 export default {
-    data(){
-        return{
+    data() {
+        return {
 
         }
     },
-    mounted(){
+    mounted() {
         this.$store.dispatch('demo/invokePushItems')
     }
 }
 </script>
 <style lang="scss" scoped>
+.main {
+    .main-content {
+        font-size: 40px;
+    }
+}
 </style>
