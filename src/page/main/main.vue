54189ae7d6089f42236149aef7fa5a996a851a37
xulianjie@landawang.com <xulianjie@landawang.com>
2019-04-25 15:08:59 +0800

    微信授权

--- src/page/main/main.vue
+++ src/page/main/main.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="main">
-        main
-        <img src="../../assets/img/avator.jpg"
-             alt="">
+        <div>
+             用户信息openid：   {{this.userInfo.openid}}  <br/>
+             headimgurl : {{this.userInfo.headimgurl}} <br/>
+             nickname :{{this.userInfo.nickname}} <br/>
+            sex: {{this.userInfo.sex}} <br/>
+             ts :{{this.userInfo.ts}} <br/>
+            unionid: {{this.userInfo.unionid}} <br/>
+        </div>
     </div>
 </template>
 <script>
 export default {
+    data() {
+        return {
+            userInfo:{}
+        }
+    },
     mounted() {
+        this.userInfo = this.$store.state.userInfo.userInfoDetail
         this.$request('wechartClass').then(res => {
             console.log(res);
         })
